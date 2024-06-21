9588ea4fe562ea2245313642292e0797cb1bb25b
fthatnathan3 <flamenathaniel3@gmail.com>
2024-06-21 20:54:23 +0700

    go back xXRevert "Just finish adding up the input script"
    This reverts commit ea694f4465a265c34acd2883d47247d4647a0280.

--- test.js
+++ test.js
@@ -1,30 +1,4 @@
-const textbox = document.querySelectorAll(".textbox");
-addEventListener("keydown", keyEvent);
-console.log(textbox.length);
-textbox[12].style.color="blue";
-let indexNum = 0;
-
-function keyEvent(event){
-    const key = event.key;
-    if (key == "Enter"){
-        checkFunction(indexNum);
-    }
-    else if ((event.keyCode > 64) && (event.keyCode < 91)) {
-        textbox[indexNum].innerHTML = `${key}`;
-        indexNum++;
-        console.log(`${event.keyCode}`);
-    }
-    else{
-        return;
-    }
-}
-
-function checkFunction(indexNum){
-    if((indexNum%5) != 0){
-        console.log("check again");
-    }
-    else {
-        indexNum++;
-    }
-}
-
+let foo = document.querySelector("#test");
+console.log(foo.getAttribute("data-checkSum"));
+foo.setAttribute("data-checkSum", "2")
+console.log(foo.getAttribute("data-checkSum"));
